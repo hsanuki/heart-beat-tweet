58f0028dcebde5fa69323132f2d1a67b507d78db
hsanuki <sanukiharuyukisanuki@gmail.com>
2017-01-05 03:19:26 +0900

    [fix] auth_fitbit2 login

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
-
-  def after_confirmation_path_for(resource_name, resource)
-    session[:user_id] = resource[:id]
-    "/auth/fitbit_oauth2/"
-  end
+  # def after_confirmation_path_for(resource_name, resource)
+  #   # session[:user_id] = resource[:id]
+  #   "/"
+  # end
 end
