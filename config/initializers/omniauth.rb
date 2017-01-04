57670ef28d72052fa4aff516e6a64d582bbf1b91
hsanuki <sanukiharuyukisanuki@gmail.com>
2017-01-05 03:57:11 +0900

    [fix] http→https

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :fitbit_oauth2, "227YDT", "d2c2fdd07deb12a78f2b6cbed91dbbcf", response_type: 'code', scope: 'profile heartrate', redirect_uri: "http://localhost:3000/auth/fitbit_oauth2/callback", expires: false
   # provider :fitbit_oauth2, "227YDT", "d2c2fdd07deb12a78f2b6cbed91dbbcf", response_type: 'code', scope: 'profile heartrate', redirect_uri: "http://localhost:3000/auth/fitbit_oauth2/callback", expires_in: "259200"
   # heroku update
-  provider :fitbit_oauth2, ENV["FITBIT_API_KEY"], ENV["FITBIT_API_SECRET"], response_type: 'code', scope: 'profile heartrate', redirect_uri: "http://heart-beat-tweet.herokuapp.com/auth/fitbit_oauth2/callback", expires_in: "259200"
+  provider :fitbit_oauth2, ENV["FITBIT_API_KEY"], ENV["FITBIT_API_SECRET"], response_type: 'code', scope: 'profile heartrate', redirect_uri: "https://heart-beat-tweet.herokuapp.com/auth/fitbit_oauth2/callback", expires_in: "259200"
 end
